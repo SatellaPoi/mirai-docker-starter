2ee9be99ebc53d8260c35b968d845a836c9fbf84
Satella_Poi <moonlightschina@gmail.com>
2022-11-02 17:01:48 +0800

    Update.

--- mcl/docs/.conf/nav.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    text: 'mirai-console-loader',
-    link: '/',
-    items: [
-        {text: '主页', link: '/'},
-        {text: 'MCL模块', link: '/Module.html'},
-        {text: 'MCL插件', link: '/Plugin.html'},
-    ],
-};
